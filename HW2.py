cf35b0579361707d290213c11625455a59542a48
Marissha <balabobalaba71@gmail.com>
2024-07-22 13:14:17 +0300

    Add files via upload

---
+++ HW2.py
@@ -0,0 +1,5 @@
+home_work = 12
+time_spent = 1.5
+course_name = 'Python'
+lead_time = (time_spent/home_work)
+print('Курс: ', course_name, ', всего задач: ', home_work, ', затрачено часов: ', time_spent, ', среднее время выполнения ', lead_time, ' часа.')
